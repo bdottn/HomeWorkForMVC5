f9cdc2fd12a06267ff31c4cff957d2ea54e3c832
Echo Lee <bdtoon.lee+account.git@gmail.com>
2015-11-24 11:58:19 +0800

    建立「客戶清單」Controller，並產生 CRUD 方法及頁面，由於「客戶清單」為檢視表，僅有檢視功能，移除不必要之方法及頁面 修改 Layout 連結，建立「客戶清單列表」連結到「客戶清單」的瀏覽頁面 修改 Route，變更預設 Controller 為「客戶清單」

---
+++ HomeWorkForMVC5/HomeWorkForMVC5/Controllers/客戶清單Controller.cs
@@ -0,0 +1,26 @@
+﻿using HomeWorkForMVC5.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HomeWorkForMVC5.Controllers
+{
+    public class 客戶清單Controller : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: 客戶清單
+        public ActionResult Index()
+        {
+            return View(db.客戶清單.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
