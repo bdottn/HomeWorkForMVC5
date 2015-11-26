3d9ffc10baad9a6dd5c9db40e193e1d0e8c95571
Echo Lee <bdtoon.lee+account.git@gmail.com>
2015-11-26 14:05:41 +0800

    加入 TODO 事項

--- HomeWorkForMVC5/HomeWorkForMVC5/Models/CellPhoneAttribute.cs
+++ HomeWorkForMVC5/HomeWorkForMVC5/Models/CellPhoneAttribute.cs
@@ -7,6 +7,8 @@ namespace HomeWorkForMVC5.Models
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public sealed class CellPhoneAttribute : DataTypeAttribute
     {
+        // TODO 練習繼承ValidationAttribute 自訂欄位驗證，投影片 P.30
+
         private static Regex regex = new Regex(@"^\d{4}-\d{6}$", RegexOptions.IgnoreCase);
 
         public CellPhoneAttribute()
